dc84cc53788d8aabc12f0ded42b5072e19431417
Kyle Lape <kyle.lape@redhat.com>
2013-10-18 11:18:00 -0700

    minor fix to test script

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 curl -H "Content-Type: application/json" -d '{"firstName":"Kyle","lastName":"Lape","email":"kyle.lape@gmail.com"}' http://localhost:8080/time/services/user
 curl -H "Content-Type: application/json" -d '{"name":"Case work"}' http://localhost:8080/time/services/action
 curl -H "Content-Type: application/json" -d '{"user":{"id":"1"},"action":{"id":"2"}}' http://localhost:8080/time/services/entry
-curl -H "Accept: application/json" http://localhost:8080/time/services/entry
+curl -H "Accept: application/json" http://localhost:8080/time/services/entry/3
